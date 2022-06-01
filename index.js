0fa1fe77741637765d2b08516a851c01637f59e1
Shabex <shabsibrah@gmail.com>
2022-06-01 21:07:00 +0300

    commit tree 357b60b08b4ecf293d8fbb0614fb07414fd19de2

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    return cats.push ('Ralph')
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift ('Bob');
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop ('Garfield')
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift ('Milo')
+}
+function appendCat(name){
+    const name_1 =[...cats, 'Broom']
+    return name_1
+
+}
+function prependCat(name){
+    const name_1 =['Arnold', ...cats]
+    return name_1
+}
+function removeLastCat(){
+    const kitten = cats.slice(0,cats.length -1)
+    return kitten
+}
+function removeFirstCat(){
+    const kiten = cats.slice(1)    
+    return kiten
+}
